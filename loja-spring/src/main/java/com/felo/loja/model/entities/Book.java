6c2a9d121393682fd20b9837685ad4eb288ced69
Felo <felipemelo713@gmail.com>
2023-07-19 10:32:02 -0300

    Adicionadas as classes de livros e clientes e feita conexão com o banco de dados postgres

---
+++ loja-spring/src/main/java/com/felo/loja/model/entities/Book.java
@@ -0,0 +1,86 @@
+package com.felo.loja.model.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="TB_BOOK")
+public class Book implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String autor;
+	private BigDecimal price;
+	
+	public Book() {
+	
+	}
+
+	public Book(Long id, String name, String autor, BigDecimal price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.autor = autor;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
